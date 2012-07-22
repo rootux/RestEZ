94142fe5a649b858c79737d64d0864e1e745a4a7
yarden shem tov <yarden@taykey.com>
2012-07-22 20:15:06 +0300

    building the basic list and add it to the homepage

--- restez-client/src/main/java/com/demo/restez/restlist/DefaultResturantsList.java
+++ restez-client/src/main/java/com/demo/restez/restlist/DefaultResturantsList.java
@@ -6,8 +6,10 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.cellview.client.CellList;
+import com.google.gwt.user.cellview.client.HasKeyboardSelectionPolicy.KeyboardSelectionPolicy;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.ListDataProvider;
 
 public class DefaultResturantsList extends Composite implements ResturantsList
 {
@@ -26,9 +28,27 @@ public class DefaultResturantsList extends Composite implements ResturantsList
 
 	public DefaultResturantsList()
 	{
+
+		  ListDataProvider<String> resturants = new ListDataProvider<String>();
+		  
+		  
+
+		resturants.getList().add("avazi");
+		resturants.getList().add("gute");
+		resturants.getList().add("McDonalds");
+		resturants.getList().add("the fat");
+		resturants.getList().add("Meat Bar");
+		
 		Cell<String> cell = new TextCell();
 		resturantList = new CellList<String>(cell);
+		resturantList.setPageSize(30);
+		resturantList.setKeyboardSelectionPolicy(KeyboardSelectionPolicy.BOUND_TO_SELECTION);
+		
+		
+
 		initWidget(uiBinder.createAndBindUi(this));
+		
+		resturants.addDataDisplay(resturantList);
 	}
 
 
