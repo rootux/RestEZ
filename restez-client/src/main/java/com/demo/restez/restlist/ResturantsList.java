d5a406fc79254f6bd58dea72471b1868c7babe30
yarden shem tov <yarden@taykey.com>
2012-07-22 19:20:51 +0300

    create resutrant list widget and presenter

---
+++ restez-client/src/main/java/com/demo/restez/restlist/ResturantsList.java
@@ -0,0 +1,13 @@
+package com.demo.restez.restlist;
+
+import com.google.gwt.user.client.ui.IsWidget;
+
+public interface ResturantsList extends IsWidget
+{
+	interface Presenter
+	{
+		
+	}
+	
+	void setPresenter(Presenter presenter);
+}
