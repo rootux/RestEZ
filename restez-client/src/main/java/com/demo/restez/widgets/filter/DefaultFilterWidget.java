e1bc8cdf6783bfc47ac27f90914831c3e719f527
itay <itay@taykey.com>
2012-07-22 18:55:48 +0300

    Created Filter widget + Average price selector widget

---
+++ restez-client/src/main/java/com/demo/restez/widgets/filter/DefaultFilterWidget.java
@@ -0,0 +1,22 @@
+package com.demo.restez.widgets.filter;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+public class DefaultFilterWidget extends Composite
+{
+
+	private static DefaultFilterWidgetUiBinder uiBinder = GWT.create(DefaultFilterWidgetUiBinder.class);
+
+	interface DefaultFilterWidgetUiBinder extends UiBinder<Widget, DefaultFilterWidget>
+	{
+	}
+
+	public DefaultFilterWidget()
+	{
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+}
