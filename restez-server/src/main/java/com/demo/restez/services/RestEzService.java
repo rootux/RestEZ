84100121092943af52026723ade1783a4bb33643
Ido Ofzer <ido@taykey.com>
2012-07-23 12:31:06 +0300

    enable filtering

--- restez-server/src/main/java/com/demo/restez/services/RestEzService.java
+++ restez-server/src/main/java/com/demo/restez/services/RestEzService.java
@@ -11,33 +11,81 @@ import com.google.inject.Inject;
 public class RestEzService
 {
 	PodamFactoryImpl podamFactory;
-	
+
 	List<Restaurant> restaurants;
-	
+
 	@Inject
 	public RestEzService(PodamFactoryImpl podamFactory)
-    {
-	    super();
-	    this.podamFactory = podamFactory;
-	    
-	    generateRests();
-    }
+	{
+		super();
+		this.podamFactory = podamFactory;
+
+		generateRests();
+	}
 
 
 
 	private void generateRests()
-    {
+	{
 		restaurants = new ArrayList<Restaurant>();
 		for (int i = 0; i < 30; i++ )
-        {
+		{
 			restaurants.add(podamFactory.manufacturePojo(Restaurant.class));
-        }
-    }
+		}
+	}
 
 
 
 	public List<Restaurant> getRestaurants(Restaurant filter)
 	{
-		return restaurants;
+		if (filter == null)
+		{
+			return restaurants;
+		}
+		
+		return filterRestaurants(filter);
 	}
+
+
+
+	private List<Restaurant> filterRestaurants(Restaurant filter)
+    {
+	    List<Restaurant> filtered = new ArrayList<Restaurant>();
+
+		for (Restaurant restaurant : restaurants)
+		{
+			boolean adRestaurant = false;
+			if (restaurant.getAddress().toLowerCase().contains(filter.getAddress().toLowerCase()))
+			{
+				adRestaurant = true;
+			}
+			if (restaurant.getAvgPrice() == filter.getAvgPrice())
+			{
+				adRestaurant = true;
+			}
+			if (restaurant.getDesc().toLowerCase().contains(filter.getDesc().toLowerCase()))
+			{
+				adRestaurant = true;
+			}
+			if (restaurant.getName().toLowerCase().contains(filter.getName().toLowerCase()))
+			{
+				adRestaurant = true;
+			}
+			if (restaurant.getRating() == filter.getRating())
+			{
+				adRestaurant = true;
+	        }
+			if (restaurant.getServiceQuality().equals(filter.getServiceQuality()))
+			{
+				adRestaurant = true;
+	        }
+			
+			if (adRestaurant)
+			{
+				filtered.add(restaurant);
+			}
+		}
+
+		return filtered;
+    }
 }
